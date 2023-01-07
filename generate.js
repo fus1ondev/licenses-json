649f85cf181a7942947e3f4b1d90d36d3500697e
fus1ondev <luke_s1220@yahoo.co.jp>
2023-01-08 02:52:56 +0900

    setup github action

---
+++ generate.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const matter = require('white-matter');
+
+const dir = 'choosealicense-com/_licenses/';
+
+const licenseFiles = fs.readdirSync(dir);
+
+
+const json = licenseFiles.map((file) => {
+  const text = fs.readFileSync(dir + file, 'utf-8');
+  const { data, content } = matter(text);
+  return {
+    ...data,
+    content,
+  }
+});
+
+fs.writeFileSync('licenses.json', JSON.stringify(json));
